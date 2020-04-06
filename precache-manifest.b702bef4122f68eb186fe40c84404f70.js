9383df590356565208a486016ec412934fdcbcc2
Rajput Rishabh Tanwar #rrt <rishabhtanwar2000@gmail.com>
2020-04-06 19:45:12 +0530

    Updates

---
+++ precache-manifest.b702bef4122f68eb186fe40c84404f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6518918e95e948bd5923515acf2b5a2",
+    "url": "/Robot-Rolodex/index.html"
+  },
+  {
+    "revision": "36fc8fc965f2a68d802b",
+    "url": "/Robot-Rolodex/static/css/main.7d547544.chunk.css"
+  },
+  {
+    "revision": "03f02f90fb0bcafe1ffd",
+    "url": "/Robot-Rolodex/static/js/2.3dfd2384.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robot-Rolodex/static/js/2.3dfd2384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36fc8fc965f2a68d802b",
+    "url": "/Robot-Rolodex/static/js/main.b7aa132d.chunk.js"
+  },
+  {
+    "revision": "0c3c41d0651a03315a54",
+    "url": "/Robot-Rolodex/static/js/runtime-main.a3da6734.js"
+  }
+]);
