af90b31a9787fb9e8e4ca6592b8e9723385d739e
Rajput Rishabh Tanwar #rrt <rishabhtanwar2000@gmail.com>
2020-04-17 16:08:03 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component{
     )
     return(
       <div className="App">
-        <h1> Robot Rolodex </h1>
+        <h1>Robot Rolodex </h1>
         <SearchBox 
            placeholder="Search Robots"
            handlechange={e => this.setState({ searchField: e.target.value })}
